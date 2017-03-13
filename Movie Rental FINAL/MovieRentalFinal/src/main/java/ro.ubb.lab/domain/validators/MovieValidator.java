8db3b80393afd6c85bce9b40b7f08e3312712760
horel <horatiu.istovan@gmail.com>
2017-03-13 18:17:50 +0200

    4th

--- Movie Rental FINAL/MovieRentalFinal/src/main/java/ro.ubb.lab/domain/validators/MovieValidator.java
+++ Movie Rental FINAL/MovieRentalFinal/src/main/java/ro.ubb.lab/domain/validators/MovieValidator.java
@@ -7,9 +7,43 @@ import ro.ubb.lab.domain.Movie;
  */
 public class MovieValidator implements Validator<Movie> {
     @Override
-    public void validate(Movie entity) throws ValidatorException;
+    public void validate(Movie entity) throws ValidatorException
     {
         String err = "";
+
+        Long id = entity.getId();
+        String name = entity.getName();
+        String genre = entity.getGenre();
+
+        if (id == null){
+            err += "ID cannot be null ";
+
+        }
+
+        if (name == null){
+            err += "Name cannot be null ";
+
+        }
+        if (genre == null){
+        err += "Genre cannot be null ";
+
+        }
+        if (id < 0){
+            err += "ID must be > 0 ";
+        }
+
+        if (name.equals("")){
+            err += "Name must be non-empty ";
+        }
+
+        if (genre.equals("")){
+            err += "Genre must be non-empty ";
+        }
+
+        if (!err.equals(""))
+        {
+            throw new ValidatorException(err);
+        }
 
 
     }
